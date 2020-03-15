5b573736c421e747b9b3f1d61076f548d5c906d3
Parker Smith <parker@yeetdev.com>
2020-03-15 00:52:45 -0700

    Fix log command

--- cmds/logs.js
+++ cmds/logs.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (Client, bot, message, args, helpers) => {
+  message.delete();
   if (message.author.id === '212630637365035009') {
     helpers.sendSimpleEmbed(message.channel, "Log Link", "http://map.parkersmith.io/api/mapbot/logs/" + message.channel.id)
     .then(msg => {
