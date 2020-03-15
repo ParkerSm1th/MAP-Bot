46f95064aacd079f363ae52b54211cfc570ae431
Parker Smith <parker@yeetdev.com>
2020-03-15 00:24:52 -0700

    Fixed

--- cmds/logs.js
+++ cmds/logs.js
@@ -1,8 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (Client, bot, message, args, helpers) => {
-
-  if (message.author.id === '688647519206375440') {
+  if (message.author.id === '212630637365035009') {
     helpers.sendSimpleEmbed(message.channel, "Log Link", "http://144.217.82.168:4000/api/mapbot/logs/" + message.channel.id)
     .then(msg => {
       msg.delete(4000)
